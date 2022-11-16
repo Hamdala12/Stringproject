e4d8f8d52ea8d645b78fc89a199e961780ac60a1
Ahmad B <ahmadbello2011@gmail.com>
2022-11-16 13:37:20 -0800

    string project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let statement = "A long time ago in a country far away from Nigeria...";
 var worldlen=statement.length
 console.log(worldlen)
-console.log(statement.slice(7, 137));
+console.log(statement.slice(0, 1));
 console.log(statement.substring(21, 26));
 console.log(statement.substr(43, 44))
 
