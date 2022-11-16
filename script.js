d88112810ec44df882af8d0d10abafd6c2b8686e
Ahmad B <ahmadbello2011@gmail.com>
2022-11-16 13:46:07 -0800

    string

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let statement = "A long time ago in a country far away from Nigeria...";
 var worldlen=statement.length
 console.log(worldlen)
+// for A
 console.log(statement.slice(0, 1));
-console.log(statement.substring(21, 26));
-console.log(statement.substr(43, 44))
-
+console.log(statement.substring(0, 1));
+console.log(statement.substr(0, 1))
+// for ago
+console.log(statement.slice(12, 16));
+console.log(statement.substring(12, 16));
+console.log(statement.substr(12, 3))
+//for country
+console.log(statement.slice(21, 29));
+console.log(statement.substring(21, 29));
+console.log(statement.substr(21, 8))
+//for away
+console.log(statement.slice(33, 38));
+console.log(statement.substring(33, 38));
+console.log(statement.substr(33, 5))
+//for Nigeria
 console.log(statement.slice(7, 137));
 console.log(statement.substring(21, 26));
 console.log(statement.substr(43, 44))
-
-console.log(statement.slice(7, 137));
-console.log(statement.substring(21, 26));
-console.log(statement.substr(43, 44))
-
-console.log(statement.slice(7, 137));
-console.log(statement.substring(21, 26));
-console.log(statement.substr(43, 44))
-
-console.log(statement.slice(7, 137));
-console.log(statement.substring(21, 26));
-console.log(statement.substr(43, 44))
-
+//for ...
 console.log(statement.slice(7, 137));
 console.log(statement.substring(21, 26));
 console.log(statement.substr(43, 44))
