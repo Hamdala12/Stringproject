c48843e8714092fe538e4f10b96be57810265635
Ahmad B <ahmadbello2011@gmail.com>
2022-11-16 12:23:20 -0800

    string

---
+++ string.js
@@ -0,0 +1,19 @@
+// let greeting = "Is today topic is String?";
+// var worldlen=greeting.length
+// console.log(worldlen)
+// // console.log(greeting.slice(3, 24))
+
+// console.log(greeting.slice(-5));
+
+let qoute = "If you do not have a goal you are not better than a goat";
+var worldlen=qoute.length
+
+ console.log(worldlen)
+
+ console.log(qoute.slice(7, 137));
+
+ console.log(qoute.substring(21, 26));
+
+ console.log(qoute.slice(38, 45));
+
+ console.log(qoute.substr(-4,4));
